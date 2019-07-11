40f01a7ba62ac670cec08b55b6c3091572b41599
Isaac Zinda <isaac@lightstep.com>
2019-07-11 09:32:18 -0700

    add client to forward report_requests to satellites

--- boost_example.cpp
+++ boost_example.cpp
@@ -17,11 +17,153 @@ taken from: http://think-async.com/Asio/boost_asio_1_13_0/doc/html/boost_asio/tu
 
 #include <sstream>
 
-#define PORT 1998
+#define SERVER_PORT 1998
+#define SATELLITE_IP "127.0.0.1"
+#define SATELLITE_PORT "8360"
 
 using boost::asio::ip::tcp;
 namespace beast = boost::beast;
 namespace http = beast::http;
+namespace net = boost::asio;
+
+// Report a failure
+void
+fail(beast::error_code ec, char const* what)
+{
+    std::cerr << what << ": " << ec.message() << "\n";
+}
+
+// Performs an HTTP POST and prints the response
+class session : public std::enable_shared_from_this<session>
+{
+    tcp::resolver resolver_;
+    beast::tcp_stream stream_;
+    beast::flat_buffer buffer_; // (Must persist between reads)
+    http::request<http::string_body> req_;
+    http::response<http::string_body> res_;
+
+public:
+    // Objects are constructed with a strand to
+    // ensure that handlers do not execute concurrently.
+    explicit
+    session(net::io_context& ioc)
+        : resolver_(net::make_strand(ioc))
+        , stream_(net::make_strand(ioc))
+    {
+    }
+
+    // Start the asynchronous operation
+    void
+    run(
+        char const* host,
+        char const* port,
+        char const* target,
+        int version,
+        std::string body)
+    {
+        // Set up an HTTP POST request message
+        req_.version(version);
+        req_.method(http::verb::post);
+        req_.target(target);
+
+        req_.set(http::field::host, host);
+        req_.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+        // standard headers for sending to satellites
+        req_.insert("Content-Type", "application/octet-stream");
+        req_.insert("Accept", "text/plain"); // get a human-readable response from the server
+
+        req_.body() = body;
+        req_.prepare_payload(); // sets body size in HTTP headers
+
+        // Look up the domain name
+        resolver_.async_resolve(
+            host,
+            port,
+            beast::bind_front_handler(
+                &session::on_resolve,
+                shared_from_this()));
+    }
+
+    void
+    on_resolve(
+        beast::error_code ec,
+        tcp::resolver::results_type results)
+    {
+        if(ec)
+            return fail(ec, "resolve");
+
+        // Set a timeout on the operation
+        stream_.expires_after(std::chrono::seconds(30));
+
+        // Make the connection on the IP address we get from a lookup
+        stream_.async_connect(
+            results,
+            beast::bind_front_handler(
+                &session::on_connect,
+                shared_from_this()));
+    }
+
+    void
+    on_connect(beast::error_code ec, tcp::resolver::results_type::endpoint_type)
+    {
+        if(ec)
+            return fail(ec, "connect");
+
+        // Set a timeout on the operation
+        stream_.expires_after(std::chrono::seconds(30));
+
+        std::cout << "*** POST request ***" << std::endl;
+        std::cout << req_ << std::endl;
+
+        // Send the HTTP request to the remote host
+        http::async_write(stream_, req_,
+            beast::bind_front_handler(
+                &session::on_write,
+                shared_from_this()));
+    }
+
+    void
+    on_write(
+        beast::error_code ec,
+        std::size_t bytes_transferred)
+    {
+        boost::ignore_unused(bytes_transferred);
+
+        if(ec)
+            return fail(ec, "write");
+
+        // Receive the HTTP response
+        http::async_read(stream_, buffer_, res_,
+            beast::bind_front_handler(
+                &session::on_read,
+                shared_from_this()));
+    }
+
+    void
+    on_read(
+        beast::error_code ec,
+        std::size_t bytes_transferred)
+    {
+        boost::ignore_unused(bytes_transferred);
+
+        if(ec)
+            return fail(ec, "read");
+
+        // Write the message to standard out
+        std::cout << "*** received response ***" << std::endl;
+        std::cout << res_ << std::endl;
+
+        // Gracefully close the socket
+        stream_.socket().shutdown(tcp::socket::shutdown_both, ec);
+
+        // not_connected happens sometimes so don't bother reporting it.
+        if(ec && ec != beast::errc::not_connected)
+            return fail(ec, "shutdown");
+
+        // If we get here then the connection is closed gracefully
+    }
+};
 
 /*
 we inherit from boost::enable_shared_from_this and support a shared_ptr
@@ -34,6 +176,7 @@ private:
   tcp::socket socket_;
   boost::asio::streambuf request_data_;
   std::string message_;
+  boost::asio::io_context &io_context_;
 
   beast::flat_buffer overflow_buf_; // resizable buffer of chars
   http::request<http::string_body> request_;
@@ -73,7 +216,8 @@ public:
 private:
   // private constructor because we are using enable_shared_from_this
   tcp_connection(boost::asio::io_context &io_context)
-    : socket_(io_context)
+    : socket_(io_context),
+      io_context_(io_context)
   {
   }
 
@@ -99,26 +243,36 @@ private:
       return;
     }
 
+    /*
+    pass string by value because this is a simple first implementatiokn
+    to actually make this performant we will want to stop some of this copying
+
+    all of the retry logic and stuff should be isolated to this class
+
+    11 == HTTP/1.1
+    */
+    std::make_shared<session>(io_context_)->run(SATELLITE_IP, SATELLITE_PORT, "/api/v2/reports", 11, request_.body());
+
     // convert the request body from string --> stringstream
-    std::stringstream body_stream(request_.body());
+    // std::stringstream body_stream(body_string);
+    //
+    // GOOGLE_PROTOBUF_VERIFY_VERSION;
+    //
+    // lightstep::collector::ReportRequest report_request;
+    //
+    // if (!report_request.ParseFromIstream(&body_stream)) {
+    //   std::cerr << "there was an error parsing the report request" << std::endl;
+    //   return;
+    // }
+    //
+    // std::cout << "received spans: " << report_request.spans().size() << std::endl;
 
-    GOOGLE_PROTOBUF_VERIFY_VERSION;
-
-    lightstep::collector::ReportRequest report_request;
-
-    if (!report_request.ParseFromIstream(&body_stream)) {
-      std::cerr << "there was an error parsing the report request" << std::endl;
-      return;
-    }
-
-    std::cout << "received spans: " << report_request.spans().size() << std::endl;
-
-    write_okay_response();
+    write_response("200 OK");
   }
 
-  void write_okay_response()
+  void write_response(std::string status_line)
   {
-    message_ = "HTTP/1.1 200 OK\r\n\r\n";
+    message_ = "HTTP/1.1 " + status_line + "\r\n\r\n";
 
     auto write_handler = boost::bind(
       &tcp_connection::handle_write,
@@ -142,7 +296,7 @@ public:
   tcp_server(boost::asio::io_context& io_context)
     : io_context_(io_context),
       // connect on every adddress on this machine
-      acceptor_(io_context, tcp::endpoint(tcp::v4(), PORT))
+      acceptor_(io_context, tcp::endpoint(tcp::v4(), SERVER_PORT))
   {
     std::cout << "tcp_server::tcp_server()" << std::endl;
 
